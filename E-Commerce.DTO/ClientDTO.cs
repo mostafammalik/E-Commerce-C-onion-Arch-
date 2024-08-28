22cca080511b4b58351c0d792d301ebfcc4b9367
mostafammalik <mostafammalik751@gmail.com>
2024-08-29 02:35:37 +0300

    DTO Finished

---
+++ E-Commerce.DTO/ClientDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DTO
+{
+    public class ClientDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public int password { get; set; }
+        public string address { get; set; }
+    }
+}
---
+++ E-Commerce.DTO/OrderDTO.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { set; get; }
+        public Client client { set; get; }
+        public ICollection<Product> Products { set; get; }
+        public DateTime date_time { set; get; }
+    }
+}
